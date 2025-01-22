84b3b140ac802281dc727343b54451f74bccc645
Sebola3461 <mixvideostubecontato@gmail.com>
2025-01-21 22:10:40 -0300

    test cors on prod

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     "GET, POST, PUT, DELETE, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("origin", req.originalUrl);
+  res.setHeader("Origin", req.originalUrl);
 
   if (
     String(
